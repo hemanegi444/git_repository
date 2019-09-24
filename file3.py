fc2f50d3b20106f34694561179e163ade664f1c7
hemanegi444 <hemanegi444@gmail.com>
2019-09-24 11:53:34 +0530

    first commit

---
+++ file3.py
@@ -0,0 +1,33 @@
+import pyttsx3
+engine=pyysx3('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+speak('hello')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
